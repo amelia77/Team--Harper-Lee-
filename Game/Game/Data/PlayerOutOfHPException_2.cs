21fed3aa5a6a004e28a0c57f899d72b85acc94a4
MiroslavGatsanoga <miroslavgatsanoga@gmail.com>
2014-02-25 01:21:15 +0200

    Again PlayerOutOfHPException

---
+++ Game/Game/Data/PlayerOutOfHPException_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Data
+{
+    class PlayerOutOfHPException : Exception
+    {
+        public PlayerOutOfHPException(string msg)
+            : base(msg)
+        { }
+    }
+}
